a4f37a83776ead0e03b6953d61a711716841690d
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-02-24 08:42:28 -0500

    feat(store): persist sales state and register reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,21 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore, persistReducer } from 'redux-persist';
-import localforage from 'localforage'; 
+import localforage from 'localforage';
 
 import rootSaga from './rootSaga';
 import authReducer from './slices/authSlice';
+import salesReducer from './slices/salesSlice';
 
 const persistConfig = {
   key: 'sunat-root',
   storage: localforage,
-  whitelist: ['auth']
+  whitelist: ['auth', 'sales'],
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  sales: salesReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
