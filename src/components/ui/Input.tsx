b31875eea82adfa7f545f32491863c3ab1c9b402
LavenderEdit <gercermagden@gmail.com>
2026-03-06 12:39:49 -0500

    feat: updated the input component separating the css from it

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,15 +1,16 @@
 import { forwardRef, type InputHTMLAttributes } from "react";
+import styles from "@styles/modules/sales.module.css";
 
-export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
-    ({ className = "", ...props }, ref) => {
-        return (
-            <input
-                ref={ref}
-                className={`w-full border border-slate-300 text-slate-700 text-sm font-semibold rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 block px-3 h-[38px] outline-none transition-all disabled:opacity-50 disabled:bg-slate-100 ${props.readOnly ? "bg-slate-50" : "bg-white"
-                    } ${className}`}
-                {...props}
-            />
-        );
-    }
-);
+export const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({ className = "", ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`${styles.inputGenericBase} ${className}`}
+      {...props}
+    />
+  );
+});
 Input.displayName = "Input";
